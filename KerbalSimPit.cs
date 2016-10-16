d9eb0cb69ae232a4001d0712faa42bb13e5196a7
Peter Hardy <peter@hardy.dropbear.id.au>
2016-10-16 16:48:40 +1100

    Basic addon stuff.

--- KerbalSimPit.cs
+++ KerbalSimPit.cs
@@ -2,6 +2,12 @@ using System;
 
 using UnityEngine;
 
+[KSPAddon(KSPAddon.Startup.Instantly, true)]
 public class KerbalSimPit : MonoBehaviour
 {
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+        Debug.Log("KerbalSimPit: Started.");
+    }
 }
