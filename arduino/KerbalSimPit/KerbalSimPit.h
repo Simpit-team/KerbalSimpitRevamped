7c12df3d150d9e28be01da762847d19d2d27c487
Peter Hardy <peter@hardy.dropbear.id.au>
2017-03-19 14:29:09 +1100

    Revert "Add clamp variable to limit polling to ~2ms."
    This reverts commit 721e156668f4bd6f4744f6075aab1944080584da.

--- arduino/KerbalSimPit/KerbalSimPit.h
+++ arduino/KerbalSimPit/KerbalSimPit.h
@@ -20,9 +20,6 @@ const byte SCENE_CHANGE_PACKET = 0x03;
 const byte REGISTER_PACKET = 0x03;
 const byte DEREGISTER_PACKET = 0x04;
 
-// How often the plugin should poll for serial data, in ms
-static byte CLAMP_TIME = 2;
-
 class KerbalSimPit
 {
  public:
@@ -39,7 +36,6 @@ class KerbalSimPit
   byte _inboundBuffer[32];
   byte _outboundBuffer[32];
   byte _outboundSize;
-  unsigned long _lastPoll;
 
   enum ReceiveState_t
   {
