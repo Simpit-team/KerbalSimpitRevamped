a43e5001e33652151d73a257142812668d8464c0
Peter Hardy <peter@hardy.dropbear.id.au>
2017-04-20 22:56:52 +1000

    0.4 release.

--- KerbalSimPit.version.m4
+++ KerbalSimPit.version.m4
@@ -1,5 +1,5 @@
 define(`MAJORVER', `0')dnl
-define(`MINORVER', `3')dnl
+define(`MINORVER', `4')dnl
 define(`PATCHVER', `0')dnl
 {
   "NAME": "Kerbal Sim Pit",
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision
 // numbers by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.3.0.0")]
-[assembly: AssemblyFileVersion("0.3.0.0")]
+[assembly: AssemblyVersion("0.4.0.0")]
+[assembly: AssemblyFileVersion("0.4.0.0")]
 
 // KSPAssembly identifies the assembly title and version to KSP
-[assembly: KSPAssembly ("KerbalSimPit", 0, 3)]
+[assembly: KSPAssembly ("KerbalSimPit", 0, 4)]
