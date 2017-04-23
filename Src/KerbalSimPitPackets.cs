8fd66bc8d8e54419eaf565daa4c1348acf1f1cbc
Peter Hardy <peter@hardy.dropbear.id.au>
2017-04-23 12:59:30 +1000

    Add actiongroup packetes. Decimalise packet vars.

--- Src/KerbalSimPitPackets.cs
+++ Src/KerbalSimPitPackets.cs
@@ -1,22 +1,24 @@
 public static class CommonPackets
 {
-    public static byte Synchronisation = 0x00;
-    public static byte EchoRequest = 0x01;
-    public static byte EchoResponse = 0x02;
+    public static byte Synchronisation = 0;
+    public static byte EchoRequest = 1;
+    public static byte EchoResponse = 2;
 }
 
 public static class OutboundPackets
 {
-    public static byte SceneChange = 0x03;
-    public static byte Altitude = 0x04;
+    public static byte SceneChange = 3;
+    public static byte Altitude = 4;
 }
 
 public static class InboundPackets
 {
-    public static byte RegisterHandler = 0x03;
-    public static byte DeregisterHandler = 0x04;
-    public static byte StageEvent = 0x05;
-    public static byte CAGEnable = 0x06;
-    public static byte CAGDisable = 0x07;
-    public static byte CAGToggle = 0x08;
+    public static byte RegisterHandler = 3;
+    public static byte DeregisterHandler = 4;
+    public static byte StageEvent = 5;
+    public static byte CAGEnable = 6;
+    public static byte CAGDisable = 7;
+    public static byte CAGToggle = 8;
+    public static byte ActionGroupEnable = 9;
+    public static byte ActionGroupDisable = 10;
 }
