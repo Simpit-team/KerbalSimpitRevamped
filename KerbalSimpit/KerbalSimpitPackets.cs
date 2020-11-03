471eed96fa19c2dea734407e67772962096b14d0
LRTNZ <liam@aacl.co.nz>
2020-11-04 01:48:34 +1300

    refactor: Reworked the OutboundPackets class
    This class needed to have a bit of a shuffle around to add in support
    for Xenon gas. So I took the opportunity to rearrange the way the
    packets and their IDs are defined, to make adding more of these
    much quicker down the line.
    Now, not all of the IDs will need to be reassigned, every time that
    a new OutboundPacket is added.

--- KerbalSimpit/KerbalSimpitPackets.cs
+++ KerbalSimpit/KerbalSimpitPackets.cs
@@ -9,27 +9,40 @@ namespace KerbalSimpit
 
     public static class OutboundPackets
     {
-        public static byte SceneChange = 3;
-        public static byte Altitude = 8;
-        public static byte Apsides = 9;
-        public static byte LiquidFuel = 10;
-        public static byte LiquidFuelStage = 11;
-        public static byte Oxidizer = 12;
-        public static byte OxidizerStage = 13;
-        public static byte SolidFuel = 14;
-        public static byte SolidFuelStage = 15;
-        public static byte MonoPropellant = 16;
-        public static byte ElectricCharge = 17;
-        public static byte EvaPropellant = 18;
-        public static byte Ore = 19;
-        public static byte Ablator = 20;
-        public static byte AblatorStage = 21;
-        public static byte Velocities = 22;
-        public static byte ActionGroups = 23;
-        public static byte ApsidesTime = 24;
-        public static byte TargetInfo = 25;
-        public static byte SoIName = 26;
-        public static byte Airspeed = 27;
+        // Propulsion Resources
+        public static byte LiquidFuel = 1;
+        public static byte LiquidFuelStage = 2;
+        public static byte Oxidizer = 3;
+        public static byte OxidizerStage = 4;
+        public static byte SolidFuel = 5;
+        public static byte SolidFuelStage = 6;
+        public static byte XenonGas = 7;
+        public static byte XenonGasStage = 8;
+        public static byte MonoPropellant = 9;
+        public static byte EvaPropellant = 10;
+
+        // Vessel Resources
+        public static byte ElectricCharge = 20;
+        public static byte Ore = 21;
+        public static byte Ablator = 22;
+        public static byte AblatorStage = 23;
+
+        // Vessel Movement/Postion
+        public static byte Altitude = 30;
+        public static byte Velocities = 31;
+        public static byte Airspeed = 32;
+        public static byte Apsides = 33;
+        public static byte ApsidesTime = 34;
+
+        // Vessel Details
+        public static byte ActionGroups = 40;
+        // public static byte DeltaVTotal = ;
+        // public static byte DeltaVStage = ;
+
+        // External Environment
+        public static byte TargetInfo = 50;
+        public static byte SoIName = 51;
+        public static byte SceneChange = 52;   
     }
 
     public static class InboundPackets
