a29914e84acf9229365dc0fc732da40e013648f9
Patrick Bechon <patrick@bechon.eu>
2021-01-21 22:37:23 +0100

    Skeleton for a GUI (not functionnal yet)

---
+++ KerbalSimpit/Toolbar.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using KSP.UI.Screens;
+
+
+namespace KerbalSimpit
+{
+
+	[KSPAddon(KSPAddon.Startup.MainMenu, true)]
+	public class AppButton : MonoBehaviour
+	{
+		const ApplicationLauncher.AppScenes buttonScenes = ApplicationLauncher.AppScenes.SPACECENTER | ApplicationLauncher.AppScenes.FLIGHT;
+		private static ApplicationLauncherButton button = null;
+
+		public static Callback Toggle = delegate { };
+
+		static bool buttonVisible
+		{
+			get
+			{
+				return true;
+			}
+		}
+
+		public static void UpdateVisibility()
+		{
+			if (button != null)
+			{
+				button.VisibleInScenes = buttonVisible ? buttonScenes : 0;
+			}
+		}
+
+		private void onToggle()
+		{
+			Toggle();
+		}
+
+		public void Start()
+		{
+			GameObject.DontDestroyOnLoad(this);
+			GameEvents.onGUIApplicationLauncherReady.Add(OnGUIAppLauncherReady);
+		}
+
+		void OnDestroy()
+		{
+			GameEvents.onGUIApplicationLauncherReady.Remove(OnGUIAppLauncherReady);
+		}
+
+		void OnGUIAppLauncherReady()
+		{
+			if (ApplicationLauncher.Ready && button == null)
+			{
+				var tex = GameDatabase.Instance.GetTexture("KerbalSimpit/PluginData/icon", false);
+				button = ApplicationLauncher.Instance.AddModApplication(onToggle, onToggle, null, null, null, null, buttonScenes, tex);
+				UpdateVisibility();
+			}
+		}
+	}
+
+
+	[KSPAddon(KSPAddon.Startup.Flight, false)]
+	public class Toolbar_Flight : MonoBehaviour
+	{
+		public void Awake()
+		{
+			AppButton.Toggle += WindowFlight.ToggleGUI;
+		}
+
+		void OnDestroy()
+		{
+			AppButton.Toggle -= WindowFlight.ToggleGUI;
+		}
+	}
+
+	[KSPAddon(KSPAddon.Startup.SpaceCentre, false)]
+	public class Toolbar_SpaceCenter : MonoBehaviour
+	{
+		public void Awake()
+		{
+			AppButton.Toggle += WindowSpaceCenter.ToggleGUI;
+		}
+
+		void OnDestroy()
+		{
+			AppButton.Toggle -= WindowSpaceCenter.ToggleGUI;
+		}
+	}
+}
