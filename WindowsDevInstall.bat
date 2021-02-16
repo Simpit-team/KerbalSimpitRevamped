b54effdcf029e300456de1d0b572ac8b51f3d4e6
LRTNZ <liam@aacl.co.nz>
2021-02-16 17:22:19 +1300

    refactor: Commented the Windows Install Script

--- WindowsDevInstall.bat
+++ WindowsDevInstall.bat
@@ -1,18 +1,11 @@
 @echo off
+
+:: Ensures the install variable is populated before use.
 setlocal enabledelayedexpansion
-set KSPDIR=KerbalSpaceProgram
+
+:: Points to the install symlink, enabling this script to be installation agnostic when it comes to the game install location.
 set INSTALL=install\KerbalSimpit\
 
-@REM if exist %INSTALL% (
-@REM     echo Install Directory Exists
-@REM ) else (
-@REM     echo Missing Install Directory - Creating it now
-@REM     mkdir %INSTALL%
-@REM )
-
+:: Copies the mod files over to the required installation locations - update with new additions as required.
 robocopy  Bin %INSTALL% KerbalSimpit.dll
 robocopy  Localisation %INSTALL%\Localisation
-::cd %KSPDIR%
-::echo %KSPDIR%
-::dir
-::echo %ksp_dir%
