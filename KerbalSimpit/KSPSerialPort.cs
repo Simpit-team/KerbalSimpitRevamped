a173e0bb1c2a34d121fa2640c918ee7fd3f0cf5d
LRTNZ <liam@aacl.co.nz>
2021-10-28 01:04:43 +1300

    chore: Fixed spelling of controller

--- KerbalSimpit/KSPSerialPort.cs
+++ KerbalSimpit/KSPSerialPort.cs
@@ -33,10 +33,10 @@ namespace KerbalSimpit.Serial
         public enum ConnectionStatus
         {
             CLOSED, // The port is closed, SimPit does not use it.
-            WAITING_HANDSHAKE, // The port is opened, waiting for the controler to start the handshake
+            WAITING_HANDSHAKE, // The port is opened, waiting for the controller to start the handshake
             HANDSHAKE, // The port is opened, the first handshake packet was received, waiting for the SYN/ACK
-            CONNECTED, // The connection is established and a message was received from the controler in the last IDLE_TIMEOUT seconds
-            IDLE, // The connection is established and no message was received from the controler in the last IDLE_TIMEOUT seconds. This can indicate a failure on the controler side or a controler that only read data.
+            CONNECTED, // The connection is established and a message was received from the controller in the last IDLE_TIMEOUT seconds
+            IDLE, // The connection is established and no message was received from the controller in the last IDLE_TIMEOUT seconds. This can indicate a failure on the controller side or a controller that only read data.
             ERROR, // The port could not be openned.
         }
 
