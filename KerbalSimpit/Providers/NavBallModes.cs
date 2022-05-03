b0452f95813f174c7cc2a8c23b6c98acf323d1e0
PBechon <PBechon@users.noreply.github.com>
2022-05-03 22:26:52 +0200

    Remove unused dependancies

--- KerbalSimpit/Providers/NavBallModes.cs
+++ KerbalSimpit/Providers/NavBallModes.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using KSP.IO;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace KerbalSimpit.Providers
 {
